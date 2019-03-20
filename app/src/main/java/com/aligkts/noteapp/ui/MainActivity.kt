fb89a605a5359e33f38be7e2b107b8d260ef4e08
Ali Göktaş <ali.goktas@adesso.com.tr>
2019-03-20 09:11:59 +0300

    Commit try in bitbucket

--- app/src/main/java/com/aligkts/noteapp/ui/MainActivity.kt
+++ app/src/main/java/com/aligkts/noteapp/ui/MainActivity.kt
@@ -1,15 +1,10 @@
 package com.aligkts.noteapp.ui
 
 import android.os.Bundle
-import android.util.Log
-import android.view.View
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.Navigation
-import androidx.navigation.Navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import com.aligkts.noteapp.R
-import com.aligkts.noteapp.ui.fragment.MainFragment
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
